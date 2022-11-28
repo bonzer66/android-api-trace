d0df0114038f15545490369a68eb262f76ba641c
bonzer66 <62873327+bonzer66@users.noreply.github.com>
2022-11-27 22:54:24 -0600

    Create goTest.go

---
+++ funcTest/goTest.go
@@ -0,0 +1,40 @@
+// testing functions before adding to main package
+package main
+
+/* get flags to work 
+type Input struct {
+      
+      }
+      
+      */
+import (
+        //"flag"
+        "fmt"
+        "os"
+        "io/ioutil"
+)
+
+// takes a file as input
+func main() {
+
+        if len(os.Args) < 2 {
+        fmt.Println("Missing parameter, provide file name!")
+        return
+    }
+    data, err := ioutil.ReadFile(os.Args[1]) //os.Args[2] <<-- for sub commands
+    if err != nil {
+        fmt.Println("Can't read file:", os.Args[1])
+        panic(err)
+    }
+    fmt.Println(string(data))
+ }
+
+
+
+
+/*
+func OutputFile() {
+        *out,d1 := []byte(out,d1)
+        err := os.WriteFile()
+ }
+*/
